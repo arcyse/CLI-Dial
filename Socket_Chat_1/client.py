921e9edfe0179246d814066acdd5faabed0d688e
Archit Chadalawada <drarchitch@gmail.com>
2024-09-04 11:30:34 +0530

    Update client.py

--- Socket_Chat_1/client.py
+++ Socket_Chat_1/client.py
@@ -4,7 +4,7 @@ HEADER = 64
 PORT = 5050
 FORMAT = 'utf-8'
 DISCONNECT_MESSAGE = '!DISCONNECT'
-SERVER = '192.168.68.108'
+SERVER = '{input_your_local_IP_addr_by_IPCONFIG}'
 ADDR = (SERVER, PORT)
 
 client = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
